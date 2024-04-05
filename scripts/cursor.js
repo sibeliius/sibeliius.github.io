5c054a0a14db2e351d6950a8d081174cb09749af
sibeliius <133725829+sibeliius@users.noreply.github.com>
2024-04-06 05:23:56 +0900

    Fix custom cursor position

--- scripts/cursor.js
+++ scripts/cursor.js
@@ -1,10 +1,14 @@
 const cursor = document.querySelector('.cursor');
 
-const positionElement = (e)=> {
+const positionElement = (e) => {
   const mouseY = e.clientY;
   const mouseX = e.clientX;
-   
-  cursor.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0`;
+  const cursorWidth = cursor.offsetWidth;
+  const cursorHeight = cursor.offsetHeight;
+
+  cursor.style.left = (mouseX - (cursorWidth / 2)) + "px";
+  cursor.style.top = (mouseY - (cursorHeight / 2)) + "px";
+  cursor.style.transform = `translate3d(${mouseX - (cursorWidth / 2)}px, ${mouseY - (cursorHeight / 2)}px, 0)`;
 }
 
-window.addEventListener('mousemove', positionElement)
+window.addEventListener('mousemove', positionElement);
